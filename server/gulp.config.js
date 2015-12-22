0bd415a0954afe811d9d1cf688fefdb4d5f76cd5
jlumijarvi <janne.kr.lumijarvi@gmail.com>
2015-12-22 22:58:59 +0200

    refactoring

---
+++ server/gulp.config.js
@@ -0,0 +1,18 @@
+module.exports = function () {
+
+	var root = __dirname + '/';
+	var build = root + '../build/';
+
+    var config = {
+		root: root,
+		build: build,
+		src: root + 'src/',
+		ts: '**/*.ts',
+		data: 'data/**.*',
+		views: '**/*.html',
+		server: build + 'app.js',
+		defaultPort: 4000
+	};
+
+	return config;
+};
